d18b16e706da524c0897aa0d775ccfdc23dc4b94
ccortesm49 <ccortesm2000@gmail.com>
2024-07-21 12:52:38 -0500

    se agrego scroll para la navegacion desde el navbar

--- front/src/Components/navBar/index.jsx
+++ front/src/Components/navBar/index.jsx
@@ -1,15 +1,16 @@
+import { Link as ScrollLink } from 'react-scroll';
 import ImageLogo from "../../assets/logoNav.svg";
 import { Button } from "@/Components/ui/button";
 import { Link } from "react-router-dom";
 
-function navBar() {
+function NavBar() {
   return (
     <nav className="flex w-full h-16 px-5 lg:px-28 items-center justify-between bg-bgNavBar">
       <ul className="flex w-auto gap-2">
         <li>
           <Link to='/'>
             <div>
-              <img class="w-32 h-auto" src={ImageLogo} />
+              <img className="w-32 h-auto" src={ImageLogo} />
             </div>
           </Link>
         </li>
@@ -17,28 +18,28 @@ function navBar() {
       
       <ul className="flex gap-2 items-center">
         <li>
-        <ul className="hidden lg:flex gap-4 items-center">
-        <li>
-          <Link>
-            <p className="text-white text-sm">Inicio</p>
-          </Link>
-        </li>
-        <li>
-          <Link>
-            <p className="text-white text-sm">Sobre la Ley Justina</p>
-          </Link>
-        </li>
-        <li>
-          <Link>
-            <p className="text-white text-sm">Funcionalidades</p>
-          </Link>
-        </li>
-        <li>
-          <Link>
-            <p className="text-white text-sm">Contactanos</p>
-          </Link>
-        </li>
-      </ul>
+          <ul className="hidden lg:flex gap-4 items-center">
+            <li>
+              <ScrollLink to="inicio" smooth={true} duration={500}>
+                <p className="text-white text-sm cursor-pointer">Inicio</p>
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="leyJustina" smooth={true} duration={500}>
+                <p className="text-white text-sm cursor-pointer">Sobre la Ley Justina</p>
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="funcionalidades" smooth={true} duration={500}>
+                <p className="text-white text-sm cursor-pointer">Funcionalidades</p>
+              </ScrollLink>
+            </li>
+            <li>
+              <ScrollLink to="contactUs" smooth={true} duration={500}>
+                <p className="text-white text-sm cursor-pointer">Contactanos</p>
+              </ScrollLink>
+            </li>
+          </ul>
         </li>
         <li className="ml-4">
           <Link to="/registro">
@@ -66,5 +67,4 @@ function navBar() {
   );
 }
 
-export default navBar;
-
+export default NavBar;
--- front/src/Pages/landing/index.jsx
+++ front/src/Pages/landing/index.jsx
@@ -1,22 +1,30 @@
 import NavBar from "../../Components/navBar";
 import Footer from '../../Components/footer';
 import Inicio from '../../Components/inicio';
-import LeyJustina from '../../Components/leyJustina'
-import Funcionalidades from '../../Components/funcionalidades'
+import LeyJustina from '../../Components/leyJustina';
+import Funcionalidades from '../../Components/funcionalidades';
 import ContactUs from "../../Components/contactUs";
 import Aliados from "../../Components/aliados";
- 
+
 function LandingPage() {
   return (
     <div>
       <NavBar />
-      
+      <section id="inicio">
         <Inicio />
+      </section>
+      <section id="aliados">
         <Aliados />
-        <LeyJustina /> 
+      </section>
+      <section id="leyJustina">
+        <LeyJustina />
+      </section>
+      <section id="funcionalidades">
         <Funcionalidades />
+      </section>
+      <section id="contactUs">
         <ContactUs />
-      
+      </section>
       <Footer />
     </div>
   );
