cb6bfae39bc91d97d4df827a25946d7de73d980f
ccortesm49 <ccortesm2000@gmail.com>
2024-07-29 02:38:40 -0500

    Se agrego, home donde se da renderizado condicionado seguin las eleciones del usuario y su rol

--- front/src/Components/sideBar/index.jsx
+++ front/src/Components/sideBar/index.jsx
@@ -10,14 +10,20 @@ import logoNotas from "../../assets/iconosSidebar/notas.svg";
 import logoTratamiento from "../../assets/iconosSidebar/tratamiento.svg";
 import logoHistorial from "../../assets/iconosSidebar/historial.svg";
 
-function SideBar() {
-  const [rol, setRol] = useState("paciente");
+function SideBar({setView, rol}) {
+  
+  const [active, setActive] = useState("inicio")
   const navigate = useNavigate();
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     navigate("/");
   };
+
+  const handleSelect = (option) => {
+    setActive(option)
+    setView(option)
+  }
 
   return (
     <div className="bg-bgNavBar w-[243px] md:w-[200px] lg:w-[243px] h-full p-4 flex flex-col">
@@ -28,36 +34,55 @@ function SideBar() {
           </div>
         </Link>
         <div className="space-y-4">
-          <button className="focus:bg-inputPrimary w-full flex justify-start items-center rounded-lg px-4 py-2">
+          <button 
+          className={`${active === "inicio" ? 'bg-inputPrimary' : 'bg-bgNavBar'} w-full flex justify-start items-center rounded-lg px-4 py-2`}
+          onClick={() => handleSelect("inicio")}
+          >
             <img className="w-6 h-6" src={logoHome} alt="Inicio" />
             <p className="text-white text-sm ml-2">Inicio</p>
           </button>
-          <button className="focus:bg-inputPrimary w-full flex justify-start items-center rounded-lg px-4 py-2">
+          <button className={`${active === "calendar" ? "bg-inputPrimary" : 'bg-bgNavBar'} w-full flex justify-start items-center rounded-lg px-4 py-2`}
+            onClick={() => handleSelect("calendar")}>
             <img className="w-6 h-6" src={logoCalendar} alt="Calendario" />
             <p className="text-white text-sm ml-2">Calendario</p>
           </button>
           {rol === "medico" ? (
-            <button className="focus:bg-inputPrimary w-full flex justify-start items-center rounded-lg px-4 py-2">
+            <button 
+            className={`${active === "patient" ? "bg-inputPrimary" : 'bg-bgNavBar'} w-full flex justify-start items-center rounded-lg px-4 py-2`}
+            onClick={() => handleSelect("patient")}
+            >
               <img className="w-6 h-6" src={logoPatient} alt="Mis pacientes" />
               <p className="text-white text-sm ml-2">Mis pacientes</p>
             </button>
           ) : rol === "paciente" ? (
             <>
-              <button className="focus:bg-inputPrimary w-full flex justify-start items-center rounded-lg px-4 py-2">
+              <button 
+              className={`${active === "history" ? "bg-inputPrimary": "bg-bgNavBar"} w-full flex justify-start items-center rounded-lg px-4 py-2`}
+              onClick={() => handleSelect("history")}
+              >
                 <img className="w-6 h-6" src={logoHistorial} alt="Historial clínico" />
                 <p className="text-white text-sm ml-2">Historial clínico</p>
               </button>
-              <button className="focus:bg-inputPrimary w-full flex justify-start items-center rounded-lg px-4 py-2">
+              <button 
+              className={`${active === "treatment" ? "bg-inputPrimary": "bg-bgNavBar"} w-full flex justify-start items-center rounded-lg px-4 py-2`}
+              onClick={() => handleSelect("treatment")}
+              >
                 <img className="w-6 h-6" src={logoTratamiento} alt="Tratamientos" />
                 <p className="text-white text-sm ml-2">Tratamientos</p>
               </button>
-              <button className="focus:bg-inputPrimary w-full flex justify-start items-center rounded-lg px-4 py-2">
+              <button 
+              className={`${active === "not" ? "bg-inputPrimary": "bg-bgNavBar"} w-full flex justify-start items-center rounded-lg px-4 py-2`}
+              onClick={() => handleSelect("not")}
+              >
                 <img className="w-6 h-6" src={logoNotas} alt="Mis notas" />
                 <p className="text-white text-sm ml-2">Mis notas</p>
               </button>
             </>
           ) : null }
-          <button className="focus:bg-inputPrimary w-full flex justify-start items-center rounded-lg px-4 py-2">
+          <button 
+          className={`${active === "config" ? "bg-inputPrimary": "bg-bgNavBar"} w-full flex justify-start items-center rounded-lg px-4 py-2`}
+          onClick={() => handleSelect("config")}
+          >
             <img className="w-6 h-6" src={logoConfig} alt="Configuración" />
             <p className="text-white text-sm ml-2">Configuración</p>
           </button>
--- front/src/Pages/home/index.jsx
+++ front/src/Pages/home/index.jsx
@@ -1,12 +1,31 @@
+import { useState } from "react";
 import NavBarHome from "../../Components/navBarHome";
+import ViewPatientCalendar from './../../Components/viewPatientCalendar/Index';
+import FormPatientCrud from '../../Components/formPatientCrud/Index'
+import ViewDoctorCalendar from '../../Components/viewDoctorCalendar/Index';
+import ViewTratamiento from '../../Components/viewTratamientos/Index';
+import FormDoctorCrud from '../../Components/formDoctorCrud/Index'
 import SideBar from "../../Components/sideBar";
+import ViewPatient from "../../Components/viewPatient"
+import ViewNotas from "../../Components/viewNotas";
 function Home() {
+    const [view, setView] = useState("inicio")
+    const [rol, setRol] = useState("paciente");
+
     return(
         <div className=" h-screen flex">
-                <SideBar />
-                <div className="flex-grow">
+                <SideBar setView={setView} rol={rol} />
+                <div className="flex flex-col flex-grow">
                     <NavBarHome />
+                    {view === "patient" && <ViewPatient />}
+                    {view === "not" && <ViewNotas />}
+                    {view === "treatment" && <ViewTratamiento />}
+                    {view === "calendar" && rol === "paciente" && <ViewPatientCalendar /> }
+                    {view === "calendar" && rol === "medico" && <ViewDoctorCalendar />}
+                    {view === "confi" && rol === "medico" && <FormDoctorCrud/> }
+                    {view === "config" && rol === "paciente" && <FormPatientCrud />}
                 </div>
+                
             
         </div>
     )
