23d174dff033d8846709b673361e1dc1b339d349
ccortesm49 <ccortesm2000@gmail.com>
2024-07-30 23:28:25 -0500

    cambios de responsive en el home

--- front/src/Components/navBarHome/index.jsx
+++ front/src/Components/navBarHome/index.jsx
@@ -1,15 +1,24 @@
 
 import React from 'react'
 import bell  from '../../assets/iconHome/bell.svg'
+import Menu from '../../assets/iconosSidebar/menu.svg'
 import helpCircle from '../../assets/iconHome/help-circle.svg'
 import search from '../../assets/iconHome/search.svg'
 import { Input } from "@/Components/ui/input";
 
 
- function NavBarHome() {
+ function NavBarHome({toggleSidebar, isSidebarVisible}) {
   return (
-    <div className='w-full h-[75px] py-6 pl-6 pr-12 bg-white flex gap-10'>
-      <div className='w-9/12 flex justify-start items-center relative'>
+    <div className='w-full h-[75px] py-6 px-4 md:pl-6 md:pr-12 bg-white  flex justify-between gap-4'>
+      <img 
+      className='flex md:hidden' 
+      src={Menu}
+      alt="Menu"
+      onClick={toggleSidebar}
+
+      />
+      <div className=' w-3/5 md:w-9/12 flex justify-start items-center relative'>
+        
         <Input 
           className='w-full rounded-3xl h-full pl-12 pr-4 py-2 border border-borderCard '
           placeholder='Buscar en justina.io'
@@ -21,7 +30,7 @@ import { Input } from "@/Components/ui/input";
         />
       </div>
         
-        <div className='flex justify-evenly flex-grow items-center border-l border-inputPrimary  opacity-50'>
+        <div className='flex justify-end gap-4 md:gap-0 w-1/5 md:w-1/4 px-2 md:px-0 md:justify-evenly md:flex-grow items-center md:border-l border-inputPrimary  opacity-50'>
           <img src={bell} />
           <img src={helpCircle} />
           <div className='w-6 h-6 md:w-10 md:h-10 rounded-full bg-black'></div>
--- front/src/Pages/home/index.jsx
+++ front/src/Pages/home/index.jsx
@@ -13,79 +13,69 @@ import ViewPatientHistory from "../../Components/viewPatientHistory/Index";
 import ViewDoctorHome from "../../Components/viewDoctorHome/Index";
 
 function Home() {
-    const [view, setView] = useState("inicio");
-    const [rol, setRol] = useState("paciente");
-    const [isSidebarVisible, setSidebarVisible] = useState(false); 
-    const sidebarRef = useRef(null); 
-  
-    const toggleSidebar = () => {
-      setSidebarVisible(!isSidebarVisible);
-    };
-  
-    const handleClickOutside = (event) => {
-      if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
-        setSidebarVisible(false); 
-      }
-    };
-  
-    useEffect(() => {
+  const [view, setView] = useState("inicio");
+  const [rol, setRol] = useState("paciente");
+  const [isSidebarVisible, setSidebarVisible] = useState(false);
+  const sidebarRef = useRef(null);
 
-      if (isSidebarVisible) {
-        document.addEventListener("mousedown", handleClickOutside);
-      } else {
-        document.removeEventListener("mousedown", handleClickOutside);
-      }
-  
-      return () => {
-        document.removeEventListener("mousedown", handleClickOutside);
-      };
-    }, [isSidebarVisible]);
-  
-    return (
-      <div className="h-screen flex">
-       
-        <div className="hidden md:block w-60 flex-shrink-0">
-          <SideBar setView={setView} rol={rol} />
-        </div>
-  
-        
-        <div
-          ref={sidebarRef}
-          className={`fixed top-0 left-0 h-full w-60 z-50 bg-bgNavBar transform ${
-            isSidebarVisible ? 'translate-x-0' : '-translate-x-full'
-          } transition-transform md:hidden`}
-        >
-          <SideBar setView={setView} rol={rol} />
-        </div>
-  
-        
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible);
+  };
 
-  
-        <div className="flex flex-col flex-grow">
-        <div className="md:hidden p-4">
-          <button
-            onClick={toggleSidebar}
-            className="p-2 bg-blue-500 text-white rounded"
-          >
-            {isSidebarVisible ? "Cerrar Sidebar" : "Abrir Sidebar"}
-          </button>
-        </div>
-          <NavBarHome />
-          <div className="flex-grow p-4 overflow-y-auto">
-            {view === "patient" && <ViewPatient />}
-            {view === "not" && <ViewNotas />}
-            {view === "treatment" && <ViewTratamiento />}
-            {view === "calendar" && rol === "paciente" && <PatientCalendar />}
-            {view === "calendar" && rol === "medico" && <ViewDoctorCalendar />}
-            {view === "confi" && rol === "medico" && <FormDoctorCrud />}
-            {view === "config" && rol === "paciente" && <FormPatientCrud />}
-            {view === "inicio" && rol === "paciente" && <ViewPatientHome />}
-            {view === "inicio" && rol === "medico" && <ViewDoctorHome />}
-            {view === "history" && rol === "paciente" && <ViewPatientHistory />}
-          </div>
+  const handleClickOutside = (event) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(event.target)) {
+      setSidebarVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isSidebarVisible) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isSidebarVisible]);
+
+  return (
+    <div className="h-screen flex">
+      <div className="hidden md:block w-60 flex-shrink-0">
+        <SideBar setView={setView} rol={rol} />
+      </div>
+
+      <div
+        ref={sidebarRef}
+        className={`fixed top-0 left-0 h-full w-60 z-50 bg-bgNavBar transform ${
+          isSidebarVisible ? "translate-x-0" : "-translate-x-full"
+        } transition-transform md:hidden`}
+      >
+        <SideBar setView={setView} rol={rol} />
+      </div>
+
+      <div className="flex flex-col flex-grow">
+        <div className="md:hidden p-4"></div>
+        <NavBarHome
+          toggleSidebar={toggleSidebar}
+          isSidebarVisible={isSidebarVisible}
+        />
+        <div className="flex-grow p-4 overflow-y-auto">
+          {view === "patient" && <ViewPatient />}
+          {view === "not" && <ViewNotas />}
+          {view === "treatment" && <ViewTratamiento />}
+          {view === "calendar" && rol === "paciente" && <PatientCalendar />}
+          {view === "calendar" && rol === "medico" && <ViewDoctorCalendar />}
+          {view === "confi" && rol === "medico" && <FormDoctorCrud />}
+          {view === "config" && rol === "paciente" && <FormPatientCrud />}
+          {view === "inicio" && rol === "paciente" && <ViewPatientHome />}
+          {view === "inicio" && rol === "medico" && <ViewDoctorHome />}
+          {view === "history" && rol === "paciente" && <ViewPatientHistory />}
         </div>
       </div>
-    );
-  }
-  
-  export default Home;
+    </div>
+  );
+}
+
+export default Home;
